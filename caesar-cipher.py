9a6fe474a009b0f2f092b329c8f9a8c5cf1c7417
Oğuzhan Yılmaz <oguzhan06_61@hotmail.com>
2023-08-17 15:46:28 +0300

    functions optimized

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,54 +1,73 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(text, shift):
-    new_text = []
-    for i in text:
-        # To fix the spacing error
-        if i == " ":
-            i = " "
-        else:
-            x = alphabet.index(i) + shift
-            # To fix list out of range error
-            if x > 25:
-                x -= 26
-                i = alphabet[x]
-            else:
-                i = alphabet[x]
-        new_text.append(i)
-    # To combine encrypted letters
-    concat_text = "".join(new_text)
-    print("The encoded text is : " + concat_text)
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for letter in start_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        end_text += alphabet[new_position]
+    print(f"The {cipher_direction} text is {end_text}")
 
-def decode(text, shift):
-    new_text = []
-    for i in text:
-        # To fix the spacing error
-        if i == " ":
-            i = " "
-        else:
-            x = alphabet.index(i) - shift
-            # To fix list out of range error
-            if x < 0:
-                x += 26
-                i = alphabet[x]
-            else:
-                i = alphabet[x]
-        new_text.append(i)
-    # To combine encrypted letters
-    concat_text = "".join(new_text)
-    print("The decoded text is : " + concat_text)
-
-if direction == "encode":
-    encrypt(text, shift)
-elif direction == "decode":
-    decode(text,shift)
-
+caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
 
 # Second way
+# alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+# text = input("Type your message:\n").lower()
+# shift = int(input("Type the shift number:\n"))
+
+# def encrypt(text, shift):
+#     new_text = []
+#     for i in text:
+#         # To fix the spacing error
+#         if i == " ":
+#             i = " "
+#         else:
+#             x = alphabet.index(i) + shift
+#             # To fix list out of range error
+#             if x > 25:
+#                 x -= 26
+#                 i = alphabet[x]
+#             else:
+#                 i = alphabet[x]
+#         new_text.append(i)
+#     # To combine encrypted letters
+#     concat_text = "".join(new_text)
+#     print("The encoded text is : " + concat_text)
+
+# def decode(text, shift):
+#     new_text = []
+#     for i in text:
+#         # To fix the spacing error
+#         if i == " ":
+#             i = " "
+#         else:
+#             x = alphabet.index(i) - shift
+#             # To fix list out of range error
+#             if x < 0:
+#                 x += 26
+#                 i = alphabet[x]
+#             else:
+#                 i = alphabet[x]
+#         new_text.append(i)
+#     # To combine encrypted letters
+#     concat_text = "".join(new_text)
+#     print("The decoded text is : " + concat_text)
+
+# if direction == "encode":
+#     encrypt(text, shift)
+# elif direction == "decode":
+#     decode(text,shift)
+
+
+# Third way
 # alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
   
 # direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
